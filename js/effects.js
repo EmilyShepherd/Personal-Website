2aa897bddaf83ce884cbde7412a3a7da35252d79
Emily Shepherd <emily@emilyshepherd.me>
2015-09-14 22:38:52 +0100

    Code neatness, social media/contact and added text to casual section

--- js/effects.js
+++ js/effects.js
@@ -1,92 +1,58 @@
 
-window.onload = (function()
+(function()
 {
-    var name_section  = document.getElementById('name_section');
-    var h             = name_section.offsetHeight;
-    var w             = name_section.offsetWidth;
-    var meTop;
-    var static        = (h - 1500) / 2;
-    var name_inner    = document.getElementsByClassName('inner')[0];
-    var me            = document.getElementById('me');
-    var me_holder     = document.getElementById('meHolder');
-    var intro_section      = document.getElementById('intro_section');
-    var experience_section = document.getElementById('experience_section');
-    var volunteering_section = document.getElementById('volunteering_section');
-    var soton_bg  = document.getElementById('soton');
-    var leys_bg  = document.getElementById('theleys');
-    var links              = document.getElementsByTagName('a');
-    var nav = document.getElementById('linkHolder');
-    var nl_bg = document.getElementById('nl_bg');
-    var susu_bg = document.getElementById('susu_bg');
-    var nl_logo              = document.getElementById('nl_logo');
-    var susu_footer = document.getElementById('susu_footer');
-    var curve    = {xs: [], ys: [], ks: []};
-    var linkOverlap = 0;
+    var name_section = document.getElementById('name_section');
+    var w            = name_section.offsetWidth;
+    var me           = document.getElementById('me');
+    var links        = document.getElementsByTagName('a');
+    var nav          = document.getElementById('linkHolder');
+    var curve        = {xs: [], ys: [], ks: []};
+    var lStep        = 0;
+    
     me.style.zIndex   = 4;
     name_section.style.backgroundAttachment = '';
     
     for (var i = 0; i < links.length; i++)
     {
-        if (links[i].href.split('#').length === 2)
+        var split = links[i].href.split('#');
+        
+        if (split.length === 2)
         {
-            links[i].onclick = doClick;
+            links[i].onclick = (function(e)
+            {
+                e.preventDefault();
+                
+                var el = document.getElementById(this.href.split('#')[1]);
+                
+                scrollTo(el.winOffsetY - 180);
+            });
         }
     }
-    
-    function doClick(e)
-    {
-        e.preventDefault();
-        
-        var id = this.href.split('#')[1];
-        var el = document.getElementById(id);
-        var offset = 0;
-        
-        do
-        {
-            offset += el.offsetTop;
-            el      = el.offsetParent;
-        }
-        while (el);
-        
-        scrollTo(offset - 180);
-    }
-    
-    window.onbeforeprint = (function()
-    {
-        nl_logo.src = '/images/nl_print_logo.png';
-        me.style.position = '';
-    });
-    
-    window.onafterprint = (function()
-    {
-        nl_logo.src = '/images/nightline.png';
-        onscroll();
-    });
 
-    var onscroll = window.onscroll = (function()
+    window.onscroll = (function()
     {
         if (window.matchMedia('(max-width: 780px)').matches) return;
         
         var s = window.scrollY;
         
-        var linkOffset = 0;
-        var lStep      = linkOverlap / (document.body.offsetHeight - h);
-        linkOffset     = lStep * s;
-        nav.style.right = linkOffset + 'px';
+        // LINKS
+        var lOffset     = lStep * s;
+        nav.style.right = lOffset + 'px';
         
-        nav.parentElement.style.backgroundPositionX = CSPL.evalSpline(s, curve.xs, curve.ys, curve.ks) - linkOffset + 'px';
+        // Section Chevron
+        nav.parentElement.style.backgroundPositionX =
+            CSPL.evalSpline(s, curve.xs, curve.ys, curve.ks) - lOffset + 'px';
         
-        me.style.position = 'fixed';
+        // Nightline Logo
+        var vSection    = document.getElementById('volunteering_section');
+        var nl          = document.getElementById('nl_logo');
+        nl.style.bottom = Math.max(-100, (s - vSection.offsetTop) * 0.2) + 'px';
         
-        nl_logo.style.bottom = Math.max(-100, (s - volunteering_section.offsetTop) * 0.2) + 'px';
+        // SUSU Footer
+        var susu_footer = document.getElementById('susu_footer');
+        var susu_bg     = document.getElementById('susu_bg');
         
-        if (s < experience_section.offsetTop - 200)
-        {
-            name_section.style.backgroundPositionY = (static + s * 0.8) + 'px';
-            name_inner.style.top = (s * 0.5) + 'px';
-        }
-        
-        if (s + h > susu_bg.offsetTop + susu_bg.offsetHeight)
+        if (s + window.innerHeight > susu_bg.offsetTop + susu_bg.offsetHeight - 5)
         {
             susu_footer.style.position = '';
             susu_footer.style.bottom   = '';
@@ -94,14 +60,18 @@ window.onload = (function()
         else
         {
             susu_footer.style.position = 'fixed';
-            susu_footer.style.bottom   = Math.min(0, s + h - susu_bg.offsetTop - 30) + 'px';
+            susu_footer.style.bottom   = Math.min(0, s + window.innerHeight - susu_bg.offsetTop - 30) + 'px';
         }
         
-        if (s < intro_section.offsetTop - 200)
+        // Me Icon
+        
+        me.style.position = 'fixed';
+        
+        if (s < window.innerHeight - 200)
         {
-            var step       = (63 - meTop) / (intro_section.offsetTop - 200);
-            var newWidth   = 280 - (180 * (s / (intro_section.offsetTop - 200)));
-            me.style.top   = meTop + (s * step) + 'px';
+            var step       = (63 - me.top) / (window.innerHeight - 200);
+            var newWidth   = 280 - (180 * (s / (window.innerHeight - 200)));
+            me.style.top   = me.top + (s * step) + 'px';
             me.style.width = newWidth + 'px';
             me.style.left  = ((w - newWidth) / 2) + 'px';
         }
@@ -113,15 +83,15 @@ window.onload = (function()
         }
     });
     
-    window.onresize = (function()
+    window.onresize = window.onload = (function()
     {
-        h = name_section.offsetHeight;
-        w = name_section.offsetWidth;
-        meTop = ((document.getElementById('name_section').offsetHeight - document.getElementsByClassName('inner')[0].offsetHeight - 150) / 2) + 120;
+        var innerH = document.getElementsByClassName('inner')[0].offsetHeight;
+        w          = name_section.offsetWidth;
+        me.top     = (name_section.offsetHeight - innerH - 150) / 2 + 120;
         
-        var links = nav.children;
-        var linkWidth = 0;
-        curve     = {xs: [], ys: [], ks: []};
+        var links  = nav.children;
+        var linkW  = 0;
+        curve      = {xs: [], ys: [], ks: []};
         
         for (var i = 0; i < links.length; i++)
         {
@@ -135,9 +105,9 @@ window.onload = (function()
             }
             while (el);
             
-            var y = 0;
-            el    = links[i];
-            linkWidth += el.offsetWidth;
+            var y  = 0;
+            el     = links[i];
+            linkW += el.offsetWidth;
             
             do
             {
@@ -150,14 +120,15 @@ window.onload = (function()
             curve.ys.push(y);
         }
         
-        curve.ys.push(linkWidth);
-        curve.xs.push(document.body.offsetHeight - h);
+        curve.ys.push(linkW);
+        curve.xs.push(document.body.offsetHeight - window.innerHeight);
         
         window.CSPL.getNaturalKs(curve.xs, curve.ys, curve.ks);
         
-        linkOverlap = Math.max(0, linkWidth - w + 40);
+        var over = Math.max(0, linkW - w + 40);
+        lStep    = over / (document.body.offsetHeight - window.innerHeight);
         
-        onscroll();
+        window.onscroll();
     });
     
     function scrollTo(y, duration)
@@ -167,7 +138,7 @@ window.onload = (function()
         var diff      = y - start;
         var startTime = Date.now();
         duration      *= 1000;
-                
+        
         function doScroll()
         {
             var time     = (Date.now() - startTime) / duration;
@@ -197,5 +168,4 @@ window.onload = (function()
     });
     
     console.niceLog('Hi(re me), I\'m Emily.');
-    window.onresize();
-});
+})();
--- js/parallax.js
+++ js/parallax.js
@@ -18,16 +18,57 @@ window.Em = window.Em || { };
         
         for (var i = 0; i < els.length; i++)
         {
+            var vPos  = els[i].winOffsetY;
+            
+            // Check whether this object is in view
+            // (If not, rendering it is a waste of time)
+            if (s + window.innerHeight < vPos || s > vPos + els[i].offsetHeight)
+            {
+                continue;
+            }
+            
             var style = this.elData(els[i], 'style');
             var scale = this.elData(els[i], 'scale');
             
-            els[i].style[style] = s * scale + 'px';
+            if (style !== 'backgroundPositionY' && style !== 'backgroundPositionX')
+            {
+                els[i].style[style] = s * scale + 'px';
+            }
+            else
+            {
+                var cStyle  = window.getComputedStyle(els[i]);
+                var bg      = cStyle.backgroundImage;
+                var size    = cStyle.backgroundSize;
+                var img     = new Image();
+
+                if (bg.substr(0, 3) === 'url')
+                {
+                    img.src = bg.replace(/url\((['"])?(.*?)\1\)/gi, '$2');
+                }
+
+                switch (size)
+                {
+                    case 'cover':
+                        processCover(img, els[i]);
+                        break;
+
+                    case 'contain':
+                        processContain(img, els[i]);
+                        break;
+                }
+
+                var static = (els[i].offsetHeight - img.height) / 2;
+                var midP   = vPos + els[i].offsetHeight / 2;
+                var offset = (s + window.innerHeight / 2 - midP) * scale;
+
+                els[i].style[style] = static + offset + 'px';
+            }
         }
     });
     
     e.Parallax.prototype.elData = (function(el, name)
     {
-        return el.data('parallax-' + name) || this[name];
+        return el.data('parallax' + name.ucfirst()) || this[name];
     });
     
     e.Parallax.prototype.registerListener = (function()
@@ -72,4 +113,91 @@ window.Em = window.Em || { };
 
         return total;
     }));
+    
+    String.prototype.ucfirst = function()
+    {
+        return this.charAt(0).toUpperCase() + this.slice(1);
+    }
+    
+    function processCover(img, el)
+    {
+        var wDiff = img.width - el.offsetWidth;
+        var hDiff = img.height - el.offsetHeight;
+
+        // Needs to be scaled up
+        if (wDiff < 0 || hDiff < 0)
+        {
+            // Width Difference is most, scale it up
+            if (wDiff > hDiff)
+            {
+                setWidth(img, el.offsetWidth);
+            }
+            // Height Difference is most, scale it up
+            else
+            {
+                setHeight(img, el.offsetHeight);
+            }
+        }
+        // Needs to be scaled down
+        else if (wDiff > 0 && hDiff > 0)
+        {
+            // Width is smallest, it can be scaled
+            if (wDiff < hDiff)
+            {
+                setWidth(img, el.offsetWidth);
+            }
+            // Height is smallest, it can be scaled
+            else
+            {
+                setHeight(img, el.offsetHeight);
+            }
+        }
+    }
+    
+    function processContain(img, el)
+    {
+        var wDiff = img.width - el.offsetWidth;
+        var hDiff = img.height - el.offsetHeight;
+
+        // Needs to be scaled up
+        if (wDiff < 0 || hDiff < 0)
+        {
+            // Width Difference is least, scale it up
+            if (wDiff < hDiff)
+            {
+                setWidth(img, el.offsetWidth);
+            }
+            // Height Difference is most, scale it up
+            else
+            {
+                setHeight(img, el.offsetHeight);
+            }
+        }
+        // Needs to be scaled down
+        else if (wDiff > 0 && hDiff > 0)
+        {
+            // Width is largest, it must be scaled
+            if (wDiff > hDiff)
+            {
+                setWidth(img, el.offsetWidth);
+            }
+            // Height is smallest, it can be scaled
+            else
+            {
+                setHeight(img, el.offsetHeight);
+            }
+        }
+    }
+    
+    function setWidth(img, newWidth)
+    {
+        img.height *= newWidth / img.width;
+        img.width   = newWidth;
+    }
+    
+    function setHeight(img, newHeight)
+    {
+        img.width *= newHeight / img.height;
+        img.height = newHeight;
+    }
 })(window.Em);
